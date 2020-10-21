b0a002de092e139929bacee567c6cfc1aeea95d8
Damian Malysza <damian.malysza@gmail.com>
2020-10-21 22:50:35 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -9,7 +9,7 @@
 def select_winner(passengers)
   # add the code snippet here!
   passengers.each do |k,v|
-    if k == :suite_a && name.start_with?("A")
+    if k == :suite_a && v.start_with?("A")
       winner = name
     end
   end
